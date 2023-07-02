5e9f9af8a17ccc220a7aac5e24b10695ad161f70
ElenaCherpakova <elena.cherpakova@gmail.com>
2023-07-02 00:52:31 -0400

    fix path

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(flash());
 app.use(helmet({ crossOriginEmbedderPolicy: false }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 const scriptSrcUrls = [
